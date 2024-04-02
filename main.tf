c61247ccb6e02b0408d3ba943b929e9c2976e70e
user1 <user1@mail.com>
2024-04-02 08:09:59 +0000

    main.tf first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+	region = "ap-south-1"
+}
+
+terraform {
+	backend "s3" {
+	bucket= "terraform--bucket"
+	key = "terraform.tfstate"
+	region = "ap-south-1"
+}
+}
+
+resource "aws_instance" "tf-repo-inst" {
+	ami = "ami-007020fd9c84e18c7"
+	instance_type= "t2.micro"
+	
+	tags= {
+	Name= "tf-repo-inst"
+}
+}
+
